27030e1ef3080bf6a36571ee58fe0bf97d8ab638
Yamasiash <51135176+Yamasiash@users.noreply.github.com>
2019-06-23 18:37:23 +0300

    Практиеская часть по экзамену c#

---
+++ FileNumUpDown/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace FileNumUpDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.lblFilename = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.lblMed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(123, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "Открыть файл";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.BtnOpenFile_Click);
+            // 
+            // lblFilename
+            // 
+            this.lblFilename.AutoSize = true;
+            this.lblFilename.Location = new System.Drawing.Point(141, 17);
+            this.lblFilename.Name = "lblFilename";
+            this.lblFilename.Size = new System.Drawing.Size(64, 13);
+            this.lblFilename.TabIndex = 1;
+            this.lblFilename.Text = "Имя файла";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSave.Location = new System.Drawing.Point(12, 66);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(123, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Location = new System.Drawing.Point(144, 30);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(55, 13);
+            this.lblMin.TabIndex = 3;
+            this.lblMin.Text = "Минимум";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Location = new System.Drawing.Point(144, 56);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(61, 13);
+            this.lblMax.TabIndex = 4;
+            this.lblMax.Text = "Максимум";
+            // 
+            // lblMed
+            // 
+            this.lblMed.AutoSize = true;
+            this.lblMed.Location = new System.Drawing.Point(144, 76);
+            this.lblMed.Name = "lblMed";
+            this.lblMed.Size = new System.Drawing.Size(52, 13);
+            this.lblMed.TabIndex = 5;
+            this.lblMed.Text = "Медиана";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 101);
+            this.Controls.Add(this.lblMed);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblFilename);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Label lblFilename;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label lblMed;
+    }
+}
+
---
+++ FileNumUpDown/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FileNumUpDown
+{
+    public partial class Form1 : Form
+    {
+        private int CountNums;
+        private List<int> Nums = new List<int>();
+        private string Path;
+        private List<NumericUpDown> Nuds = new List<NumericUpDown>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOpenFile_Click(object sender, EventArgs e)
+        {
+            Nums.Clear();
+            Nuds.Clear();
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            Path = ofd.FileName;
+            FillNums(Path);
+            int y = 50;
+            foreach (var i in Nums)
+            {
+                NumericUpDown nud = new NumericUpDown();
+                nud.Location = new Point(10, y);
+                nud.Width = 50;
+                nud.Height = 50;
+                Height += 50;
+                y += 50;
+                nud.Value = i;
+                Nuds.Add(nud);
+                Controls.Add(nud);
+                nud.ValueChanged += Nud_ValueChanged;
+                lblMin.Text = Nums.Min().ToString();
+                lblMax.Text = Nums.Max().ToString();
+                double avg = Convert.ToInt32(Nums.Sum()) / Nums.Count;
+                lblMed.Text = avg.ToString();
+            }
+            lblFilename.Text = Path;
+        }
+
+        private void Nud_ValueChanged(object sender, EventArgs e)
+        {
+            Nums.Clear();
+            foreach (var i in Nuds)
+            {
+                Nums.Add(Convert.ToInt32(i.Value));
+            }
+            lblMin.Text = Nums.Min().ToString();
+            lblMax.Text = Nums.Max().ToString();
+            double avg = Convert.ToInt32(Nums.Sum()) / Nums.Count;
+            lblMed.Text = avg.ToString();
+        }
+
+        private void FillNums(string path)
+        {
+            var nums = File.ReadAllLines(path);
+            CountNums = Convert.ToInt32(nums[0]);
+            for (int i = 1; i < nums.Length; i++)
+            {
+                Nums.Add(Convert.ToInt32(nums[i]));
+            }
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            File.Delete(Path);
+            using (StreamWriter sr = new StreamWriter(Path))
+            {
+                sr.WriteLine(Nums.Count);
+                foreach (var i in Nuds)
+                {
+                    sr.WriteLine(i.Value);
+                }
+            }
+            MessageBox.Show("Сохранено");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
